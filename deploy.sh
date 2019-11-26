ae69a8dcd1208f6849fe20cd70ce15b6b903c923
Matt Wicks <matt.wicks@gmail.com>
2019-11-26 12:24:51 +1100

    updated deploy script added github action to deploy logic apps

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,46 @@
 #!/bin/bash
-RESOURCE_GROUP_NAME="SSW.Shorts"
-TEMPLATE_FILE="arm/template.json"
-DEPLOYMENT_NAME=$(date +"%Y-%m-%d_%H-%M-%S")
-echo $DEPLOYMENT_NAME
 
-echo 'Input your SubscriptionId'
-read SUBSCRIPTION_ID
+resourceGroup=${resourceGroup:-SSW.Shorts}
+templateFile=${templateFile:-arm/template.json}
+deploymentName=${deploymentName:-$(date +"%Y-%m-%d_%H-%M-%S")}
+subscriptionId=${subscriptionId:-n/a}
+location=${location:-Australia East}
 
-az login --output none
+# exit when any command fails
+set -e
 
+# read named params
+while [ $# -gt 0 ];
+do
+
+   if [[ $1 == *"--"* ]]; then
+        param="${1/--/}"
+        declare $param="$2"
+        # echo $1 $2 // Optional to see the parameter:value result
+   fi
+
+  shift
+done
+
+if [ $subscriptionId != "n/a" ]
+then
+	echo "Setting subscription"
+	az account set --subscription $subscriptionId
+fi
+
+echo "Checking if RG $resourceGroup exists"
+if [ $(az group exists --name $resourceGroup) == false ]; 
+then
+	echo "   Doesn't exist - are you logged into the correct subscription?"
+	echo "   Tip: you can pass the subscription id with the parameter --subscriptionId"
+	exit 1
+fi
+
+echo "Deploying $deploymentName to RG $resourceGroup"
 az group deployment create \
 	--verbose \
-	--name "$DEPLOYMENT_NAME" \
-	--resource-group $RESOURCE_GROUP_NAME \
-	--template-file $TEMPLATE_FILE \
-	--subscription $SUBSCRIPTION_ID
+	--name "$deploymentName" \
+	--resource-group $resourceGroup \
+	--template-file $templateFile
+
+echo "   Done"
